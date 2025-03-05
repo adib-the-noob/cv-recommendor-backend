7fcfadb1aedf6c070677522223a3d22e3a595a6f
Mohammed Adib <mohammed.adib1104@gmail.com>
2025-03-06 00:13:45 +0600

    models and migration updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->string('profile_picture')->nullable();
+
+            $table->boolean('is_verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
+           
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
